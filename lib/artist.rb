e3cee3fc655d6597460c6f069dd4bc80354cca41
Cayla Horsey <cayla.horsey@powerhrg.com>
2024-07-15 13:40:42 -0400

    Update complete lab

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,8 +1,3 @@
-require 'pry'
-require_relative '../lib/concerns/memorable'
-require_relative '../lib/concerns/findable'
-require_relative '../lib/concerns/paramable'
-
 class Artist
   extend Memorable::ClassMethods
   include Memorable::InstanceMethods
--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,3 @@
-require 'pry'
-require_relative '../lib/concerns/memorable'
-require_relative '../lib/concerns/findable'
-require_relative '../lib/concerns/paramable'
-
 class Song
   extend Memorable::ClassMethods
   include Memorable::InstanceMethods
